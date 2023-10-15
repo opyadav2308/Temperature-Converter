489b53552044d067b727934776a626b0223c83bb
Om Prakash Yadav <142107907+opyadav2308@users.noreply.github.com>
2023-10-15 19:43:47 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,43 +4,45 @@ const degree = document.querySelector("#degree");
 const convertBtn = document.querySelector("#convert-btn");
 const tempType = document.querySelector("#temp-type");
 
+// Function to add a loading feature to the button
+function addLoadingFeature() {
+    convertBtn.innerHTML = "<span class='icon'> <i class='fa fa-spinner fa-spin'></i> Converting...</span>";
+    convertBtn.setAttribute("disabled", "true");
+
+    setTimeout(() => {
+        convertBtn.innerHTML = "<span>Convert</span>";
+        convertBtn.removeAttribute("disabled");
+    }, 1000);
+}
+
 // window loading reset
 window.addEventListener("load", () => {
     degree.value = "";
     celsiusField.innerHTML = "";
 });
 
-if (degree.value === "") {
-    convertBtn.setAttribute("disabled", "");
-    setTimeout(() => {
-        convertBtn.removeAttribute("disabled");
-    }, 4000);
-}
-
 convertBtn.addEventListener("click", (e) => {
     e.preventDefault();
     convertToCelsius();
-
-    // add loading feature 
-    convertBtn.innerHTML = "<span class='icon'> <i class='fa fa-spinner fa-spin'></i> Converting...</span>";
-    setTimeout(() => {
-        convertBtn.innerHTML = "<span>Convert</span>";
-    }, 1000)
+    addLoadingFeature();
 });
 
-// add loading
-
+// Function to convert and display the temperature
 function convertToCelsius() {
-    let inputValue = degree.Value;
+    let inputValue = parseFloat(degree.value);
+
+    if (isNaN(inputValue)) {
+        celsiusField.innerHTML = "Please enter a valid temperature";
+        return; // Exit the function early if the input is not valid
+    }
 
     setTimeout(() => {
         if (tempType.value === "fahrenheit") {
-            const fahrenheitToCelsius = (inputValue - 32) * (5 / 9);
-            celsiusField.innerHTML = `${fahrenheitToCelsius.toFixed(3)} &deg; c`;
+            const fahrenheitToCelsius = (inputValue - 32) / 1.8;
+            celsiusField.innerHTML = `${fahrenheitToCelsius.toFixed(2)} &deg;C`;
+        } else if (tempType.value === "kelvin") {
+            const kelvinToCelsius = inputValue - 273.15;
+            celsiusField.innerHTML = `${kelvinToCelsius.toFixed(2)} &deg;C`;
         }
-        else if (tempType.value === "Kelvin") {
-            const KelvinToCelsius = inputValue - 273.15;
-            celsiusField.innerHTML = `&{kelvinToCelsius.toFixed(3)} &deg;c`;
-        }
-    }, 1100)
+    }, 1100);
 }
